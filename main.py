446329bb0ef09882df882d119e8a5431e84a3bab
mshah362 <98640313+mshah362@users.noreply.github.com>
2023-04-23 20:22:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import customtkinter
-import numpy as np
-import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
+import datetime
 
 # Define custom tkinter theme
 customtkinter.set_appearance_mode("dark")
